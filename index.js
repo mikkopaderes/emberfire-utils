84562c0e47351abb6a2db7d58e58b5820af15131
Mikko Paderes <rmmpaderes@gmail.com>
2017-03-16 23:15:17 +0800

    Add base code for tree-shaking

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 /* jshint node: true */
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var featuresToExclude = [];
+
+function filterFeatures(addonConfig) {
+  addonConfig.exclude.forEach((exclude) => {
+    if (exclude === 'firebase-util') {
+      featuresToExclude.push('**/firebase-util.js');
+      featuresToExclude.push('**/has-limited.js');
+    }
+  });
+}
+
 module.exports = {
-  name: 'emberfire-utils'
+  name: 'emberfire-utils',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    var addonConfig = this.app.options[this.name];
+
+    if (addonConfig) {
+      filterFeatures(addonConfig);
+    }
+  },
+
+  treeForApp: function() {
+    var tree = this._super.treeForApp.apply(this, arguments);
+
+    return new Funnel(tree, { exclude: featuresToExclude });
+  },
+
+  treeForAddon: function() {
+    var tree = this._super.treeForAddon.apply(this, arguments);
+
+    return new Funnel(tree, { exclude: featuresToExclude });
+  },
 };
